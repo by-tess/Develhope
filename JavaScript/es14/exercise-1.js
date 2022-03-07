92266865fb9b01628ae5f77ea2872b31a62d4799
by-tess <livrierot@gmail.com>
2022-03-07 20:12:20 +0100

    push files

---
+++ JavaScript/es14/exercise-1.js
@@ -0,0 +1,22 @@
+function greet(greeting) {
+
+  function name(name) {
+    return greeting + ', ' + name;
+  }
+
+  return name;
+}
+
+const greetWithHello = greet('Hello');
+const greetWithBye = greet('Bye');
+const greetWithHi = greet('Hi');
+
+const greetPaulWithHello = greetWithHello('Paul');
+const greetPaulWithHi = greetWithHi('Paul');
+const greetJohnWithBye = greetWithBye('John');
+const greetSimonWithHello = greetWithHello('Simon');
+
+console.log(greetPaulWithHello);
+console.log(greetPaulWithHi);
+console.log(greetJohnWithBye);
+console.log(greetSimonWithHello);
